b3a80a5ced927ba2960e9d2a5f6cfda4b5f6c376
양윤직 <yang-yunjig@yang-yunjig-ui-MacBookPro.local>
2025-07-18 20:53:02 +0900

    배포중....

--- classroom-backend/src/main/java/com/example/demo/ClassroomBackendApplication.java
+++ classroom-backend/src/main/java/com/example/demo/ClassroomBackendApplication.java
@@ -1,9 +1,26 @@
+// package com.example.demo;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class ClassroomBackendApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(ClassroomBackendApplication.class, args);
+// 	}
+
+// }
+
+
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.demo.entity")
 public class ClassroomBackendApplication {
 
 	public static void main(String[] args) {
